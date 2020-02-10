33b07a129e008272f327e5687e2296b48217af8d
uqs <uqs@FreeBSD.org>
2020-02-10 16:15:34 +0000

    ... and delete the model, because we currently no longer use it.
    Coverity apparently got smarter over time and will produce fewer false
    positives around kernel malloc when this model is *not* in use.

--- model.c
+++ model.c
@@ -6,30 +6,5 @@
  * upload it via https://scan.coverity.com/projects/freebsd?tab=analysis_settings
  */
 
-/* From <sys/malloc.h>. */
-#define M_WAITOK 0x0002
 
-/*
- * If M_WAIT_OK is set, malloc() will always return something meaningful.
- */
-void *
-malloc(unsigned long size, struct malloc_type *mtp, int flags)
-{
-	int has_memory;
-
-	__coverity_negative_sink__(size);
-
-	if (flags & M_WAITOK || has_memory)
-		return __coverity_alloc__(size);
-
-	return 0;
-}
-
-/*
- * Don't complain about leaking FDs in unit tests.
- */
-static void
-leak(int fd)
-{
-	__coverity_close__(fd);
-}
+// Currently no special model is in use.
