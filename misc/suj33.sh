215011aec46d4bee8100ebc8defe688d54411830
pho <pho@FreeBSD.org>
2020-02-10 08:50:43 +0000

    Added comment about problem found. If umount fails, do not loop forever. Returen fsck exit code.

--- misc/suj33.sh
+++ misc/suj33.sh
@@ -31,6 +31,10 @@
 # Test "umount" of active file system
 # Triggers "fsync: giving up on dirty" with ease.
 
+# umount -f fails with
+# "softdep_waitidle: Failed to flush worklist for 0xfffff80308cea000"
+# Fixed by r356763
+
 [ `id -u ` -ne 0 ] && echo "Must be root!" && exit 1
 
 . ../default.cfg
@@ -50,11 +54,15 @@ su $testuser -c '(cd ..; ./run.sh disk.cfg)' > /dev/null 2>&1 &
 sleep 20
 umount $mntpoint
 kill $!
-../tools/killall.sh
+../tools/killall.sh || exit
 wait
 
-while mount | grep -q "$mntpoint "; do
-	umount $mntpoint || sleep 1
+for i in `jot 6`; do
+	mount | grep -q "on $mntpoint " || break
+	umount $mntpoint && break || sleep 10
+	[ $i -eq 6 ] &&
+	    { echo FAIL; fstat -mf $mntpoint; exit 1; }
 done
-checkfs /dev/md${mdstart}$part
+checkfs /dev/md${mdstart}$part; s=$?
 mdconfig -d -u $mdstart
+exit $s
